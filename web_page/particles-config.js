f536dd5f1b64944f617b22610cff9b17ecc45dc9
Kęstutis Butkus <exxpress@gmail.com>
2025-06-25 07:58:23 +0300

    Update particles-config.js
    200>50 points

--- web_page/particles-config.js
+++ web_page/particles-config.js
@@ -6,7 +6,7 @@ tsParticles.load("tsparticles", {
     },
     particles: {
         number: {
-            value: 200
+            value: 50
         },
         color: {
             value: "#014aa7"
